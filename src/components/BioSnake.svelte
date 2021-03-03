edb0e9e0ac61df3ba5b4b863ca63235220b23db5
StefanBoca <stefan.r.boca@gmail.com>
2021-03-02 19:00:57 -0800

    clean up code, but doesn't work lol

--- src/components/BioSnake.svelte
+++ src/components/BioSnake.svelte
@@ -1,105 +1,105 @@
 <script lang="ts">
-  import { onMount } from "svelte/internal";
+  import BioSnake from "./BioSnake";
 
-  let TICK_DELAY = 200;
-  let GRID_SIZE = 20;
-  let SNAKE_HEAD = 0;
+  let game: BioSnake = new BioSnake(20, 200, () => (lost = game.isLost)); // do something in the callback to update display
+  let lost = game.isLost;
 
-  let lost = false;
-  type Cell = "empty" | "snake" | "food";
-  let grid: Cell[][] = [...Array(GRID_SIZE)].map(() =>
-    [...Array(GRID_SIZE)].map(() => "empty")
-  );
-  let snakePosition: Array<[number, number]> = [[12, 13]];
-  let direction = [0, 1];
-  let gridWithSnake = grid;
+  // function getRandomInt(max: number) {
+  //   return Math.floor(Math.random() * Math.floor(max));
+  // }
+  // function randomFood() {
+  //   grid[getRandomInt(bs.GRID_SIZE)][getRandomInt(bs.GRID_SIZE)] = "food";
+  //   grid[getRandomInt(bs.GRID_SIZE)][getRandomInt(bs.GRID_SIZE)] = "food";
+  // }
+  // randomFood();
 
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  function randomFood() {
-    grid[getRandomInt(GRID_SIZE)][getRandomInt(GRID_SIZE)] = "food";
-    grid[getRandomInt(GRID_SIZE)][getRandomInt(GRID_SIZE)] = "food";
-  }
-  randomFood();
-  $: {
-    for (let i = 0; i < gridWithSnake.length; i++) {
-      for (let k = 0; k < gridWithSnake.length; k++) {
-        if (gridWithSnake[i][k] === "snake") {
-          gridWithSnake[i][k] = "empty";
-        }
-      }
+  // function tick(n: number) {
+  //   setTimeout(() => {
+  //     if (!started) {
+  //       return;
+  //     }
+
+  //     const [x, y] = snakePosition[bs.SNAKE_HEAD];
+  //     const [dx, dy] = direction;
+  //     const newHead = [dx + x, y + dy] as [number, number];
+  //     function isOutOfBounds(n: number) {
+  //       return n < 0 || n > bs.GRID_SIZE - 1;
+  //     }
+  //     if (isOutOfBounds(newHead[0]) || isOutOfBounds(newHead[1])) {
+  //       lost = true;
+  //       return;
+  //     }
+  //     let ateFood = false;
+  //     if (gridWithSnake[newHead[0]][newHead[1]] === "food") {
+  //       ateFood = true;
+  //       randomFood();
+  //     }
+  //     const snakeBody = snakePosition.slice(
+  //       0,
+  //       snakePosition.length - (ateFood ? 0 : 1)
+  //     );
+  //     if (snakeBody.some((x) => x[0] === newHead[0] && x[1] === newHead[1])) {
+  //       lost = true;
+  //       return;
+  //     }
+  //     snakePosition = [newHead, ...snakeBody];
+  //     prevDirection = [...direction];
+  //     updateDisplay();
+  //     tick(bs.TICK_DELAY - Math.min(snakePosition.length, 15) * 10);
+  //   }, n);
+  // }
+
+  // function restart() {
+  //   snakePosition = [[12, 13]];
+  //   direction = [0, 1];
+  //   grid = [...Array(bs.GRID_SIZE)].map(() =>
+  //     Array(bs.GRID_SIZE).fill("empty")
+  //   );
+  //   gridWithSnake = grid;
+  //   randomFood();
+  //   updateDisplay();
+  //   lost = false;
+  //   started = false;
+  // }
+
+  async function handleInput(e: { key: any }): Promise<void> {
+    if (!game.isRunning) {
+      game.start();
     }
-    snakePosition.forEach(([x, y]) => {
-      gridWithSnake[x][y] = "snake";
-    });
-  }
-  const fn = (n: number) => {
-    setTimeout(() => {
-      const [x, y] = snakePosition[SNAKE_HEAD];
-      const [dx, dy] = direction;
-      const newHead = [dx + x, y + dy] as [number, number];
-      function isOutOfBounds(n: number) {
-        return n < 0 || n > GRID_SIZE - 1;
-      }
-      if (isOutOfBounds(newHead[0]) || isOutOfBounds(newHead[1])) {
-        lost = true;
+
+    let newDir: [number, number] = [0, 0];
+    switch (e.key) {
+      case "ArrowLeft":
+        newDir = [0, -1];
+        break;
+      case "ArrowRight":
+        newDir = [0, 1];
+        break;
+      case "ArrowUp":
+        newDir = [-1, 0];
+        break;
+      case "ArrowDown":
+        newDir = [1, 0];
+        break;
+      case "Enter":
+        game.stop();
         return;
-      }
-      let ateFood = false;
-      if (gridWithSnake[newHead[0]][newHead[1]] === "food") {
-        ateFood = true;
-        randomFood();
-      }
-      const snakeBody = snakePosition.slice(
-        0,
-        snakePosition.length - (ateFood ? 0 : 1)
-      );
-      if (snakeBody.some((x) => x[0] === newHead[0] && x[1] === newHead[1])) {
-        lost = true;
-        return;
-      }
-      snakePosition = [newHead, ...snakeBody];
-      fn(TICK_DELAY - Math.min(snakePosition.length, 15) * 10);
-    }, n);
-  };
-  onMount(() => {
-    fn(TICK_DELAY);
-  });
-  function restart() {
-    snakePosition = [[12, 13]];
-    direction = [0, 1];
-    grid = [...Array(GRID_SIZE)].map(() =>
-      [...Array(GRID_SIZE)].map(() => "empty")
-    );
-    gridWithSnake = grid;
-    randomFood();
-    lost = false;
-    fn(TICK_DELAY);
+    }
+
+    if (
+      !(
+        (newDir[0] !== 0 && newDir[0] === -game.prev_direction[0]) ||
+        (newDir[1] !== 0 && newDir[1] === -game.prev_direction[1])
+      ) ||
+      game.snake.length <= 1
+    ) {
+      // opposite direction
+      game.direction = newDir;
+    }
   }
 </script>
 
-<svelte:window
-  on:keydown={(e) => {
-    switch (e.key) {
-      case "ArrowLeft":
-        direction = [0, -1];
-        break;
-      case "ArrowRight":
-        direction = [0, 1];
-        break;
-      case "ArrowUp":
-        direction = [-1, 0];
-        break;
-      case "ArrowDown":
-        direction = [1, 0];
-        break;
-      case "Enter":
-        restart();
-        break;
-    }
-  }}
-/>
+<svelte:window on:keydown={handleInput} />
 
 <main>
   {#if lost}
@@ -111,16 +111,14 @@
       > to restart
     </h3>
   {/if}
-  <h3 class="text-center">snake length {snakePosition.length}</h3>
+  <h3 class="text-center">score {game.grid.grid.length}</h3>
   <div class="flex justify-center items-center">
     <div>
-      {#each gridWithSnake as row, i}
+      {#each game.grid.grid as row, i}
         <div class="flex">
-          {#each row as cell, k}
-            <div
-              on:click={() => (grid[i][k] = "food")}
-              class={`w-5 h-5 border border-solid border-white ${cell}`}
-            />
+          {#each row as cell, j}
+            <!-- TODO: turn clicked cells into food? -->
+            <div class={`w-5 h-5 border border-solid border-white ${cell}`} />
           {/each}
         </div>
       {/each}
@@ -129,7 +127,7 @@
 
   {#if lost}
     <div class="center m-auto mt-3">
-      <button on:click={restart}> Start again </button>
+      <button on:click={game.stop}> Start again </button>
     </div>
   {/if}
 </main>
@@ -141,7 +139,10 @@
   .food {
     @apply bg-pink-500;
   }
-  .snake {
+  .snake_body {
     @apply bg-green-500;
+  }
+  .snake_head {
+    @apply bg-green-800;
   }
 </style>
