32cfc75f8c49e20dae6ac2f3b833f6b24df38410
StefanBoca <stefan.r.boca@gmail.com>
2021-03-02 22:24:44 -0800

    finally fix the thing (by rewrite, of course)

--- src/components/BioSnake.svelte
+++ src/components/BioSnake.svelte
@@ -1,77 +1,229 @@
 <script lang="ts">
-  import BioSnake from "./BioSnake";
+  const GRID_SIZE = 20;
 
-  let game: BioSnake = new BioSnake(20, 200, () => (game = game)); // hack to get this damn thing updating properly lol
+  function tick_delay(): number {
+    return 200;
+  }
 
-  async function handleInput(e: { key: any }): Promise<void> {
-    if (!game.isRunning && !game.lost) {
-      game.start();
+  type FoodCell = "empty" | "food";
+  type SnakeCell = "empty" | "head" | "body";
+
+  type Dir = "up" | "down" | "left" | "right" | "none";
+  interface State {
+    status: "stopped" | "running" | "lost";
+    dir: Dir;
+    prev_dir: Dir;
+  }
+  class Cell {
+    food: FoodCell = "empty";
+    snake: SnakeCell = "empty";
+
+    constructor() {}
+
+    get empty(): boolean {
+      return this.food == "empty" && this.snake == "empty";
     }
 
-    let newDir: [number, number] = [0, 0];
-    switch (e.key) {
-      case "ArrowLeft":
-        newDir = [0, -1];
-        break;
-      case "ArrowRight":
-        newDir = [0, 1];
-        break;
-      case "ArrowUp":
-        newDir = [-1, 0];
-        break;
-      case "ArrowDown":
-        newDir = [1, 0];
-        break;
-      case "Enter":
-        game.reset();
-        return;
-    }
-
-    if (
-      !(
-        (newDir[0] !== 0 && newDir[0] === -game.prev_direction[0]) ||
-        (newDir[1] !== 0 && newDir[1] === -game.prev_direction[1])
-      ) ||
-      game.snake.length <= 1
-    ) {
-      // opposite direction
-      game.direction = newDir;
+    get value(): FoodCell | SnakeCell {
+      // snake should always be visible above food
+      if (this.empty) return "empty";
+      return this.snake !== "empty" ? this.snake : this.food;
     }
   }
+
+  const dir2vec: Map<Dir, [number, number]> = new Map([
+    ["up", [0, -1]],
+    ["down", [0, 1]],
+    ["left", [-1, 0]],
+    ["right", [1, 0]],
+    ["none", [0, 0]],
+  ]);
+
+  const oppositeDir: Map<Dir, Dir> = new Map([
+    ["up", "down"],
+    ["down", "up"],
+    ["left", "right"],
+    ["right", "left"],
+    ["none", "none"],
+  ]);
+
+  const keyMap: Map<string, Dir> = new Map([
+    ["ArrowUp", "up"],
+    ["ArrowDown", "down"],
+    ["ArrowLeft", "left"],
+    ["ArrowRight", "right"],
+    ["w", "up"],
+    ["s", "down"],
+    ["a", "left"],
+    ["f", "right"],
+  ]);
+
+  let state: State = { status: "stopped", dir: "none", prev_dir: "none" };
+  let grid: Array<Array<Cell>>;
+  let snake: Array<[number, number]>;
+  let step: number;
+  let tick_timeout: number;
+
+  function start(): void {
+    if (state.status === "running") return;
+    if (state.dir === "none") return;
+    state.status = "running";
+    loop();
+  }
+  function stop(): void {
+    if (state.status !== "running") return;
+    state.status = "stopped";
+  }
+  function reset(): void {
+    state.status = "stopped";
+    step = 0;
+    grid = [...Array(GRID_SIZE)].map(() =>
+      [...Array(GRID_SIZE)].map(() => new Cell())
+    );
+    snake = [[12, 13]];
+    spawnFood();
+  }
+
+  $: {
+    for (let y = 0; y < GRID_SIZE; y++) {
+      for (let x = 0; x < GRID_SIZE; x++) {
+        if (grid[y][x].snake !== "empty") {
+          grid[y][x].snake = "empty";
+        }
+      }
+    }
+    snake.forEach(([x, y]) => (grid[y][x].snake = "body"));
+    grid[snake[0][1]][snake[0][0]].snake = "head";
+  }
+
+  function spawnFood(): void {
+    for (let i = 0; i < 2; i++) {
+      let unoccupied: Array<[number, number]> = [];
+      for (let y = 0; y < GRID_SIZE; y++) {
+        for (let x = 0; x < GRID_SIZE; x++) {
+          if (grid[y][x].empty) {
+            unoccupied.push([x, y]);
+          }
+        }
+      }
+      if (unoccupied.length === 0) {
+        return;
+      }
+      const rand_cell =
+        unoccupied[Math.floor(Math.random() * unoccupied.length)];
+      grid[rand_cell[1]][rand_cell[0]].food = "food";
+    }
+  }
+
+  reset();
+
+  function tick(): void {
+    const [x, y] = snake[0]; // head of snake
+    const [dx, dy] = dir2vec.get(state.dir);
+    const newHead = [dx + x, y + dy] as [number, number];
+
+    if (
+      newHead[0] < 0 ||
+      newHead[0] > GRID_SIZE - 1 ||
+      newHead[1] < 0 ||
+      newHead[1] > GRID_SIZE - 1
+    ) {
+      // out of bounds
+      state.status = "lost";
+      return;
+    }
+
+    let ateFood = false;
+    if (grid[newHead[1]][newHead[0]].food === "food") {
+      grid[newHead[1]][newHead[0]].food = "empty";
+      ateFood = true;
+      spawnFood();
+    }
+
+    const snakeBody = snake.slice(0, snake.length - (ateFood ? 0 : 1));
+
+    if (
+      snakeBody.some(
+        (x: [number, number]) => x[0] === newHead[0] && x[1] === newHead[1]
+      )
+    ) {
+      state.status = "lost";
+      return;
+    }
+
+    snake = [newHead, ...snakeBody];
+    state.prev_dir = state.dir;
+    step++;
+  }
+
+  function loop(): void {
+    tick_timeout = (setTimeout(() => {
+      tick();
+      if (state.status === "running") {
+        loop();
+      }
+    }, tick_delay()) as unknown) as number;
+  }
+
+  async function handleInput(e: KeyboardEvent): Promise<void> {
+    let new_dir: Dir = "none";
+    if (keyMap.has(e.key)) {
+      new_dir = keyMap.get(e.key);
+    } else {
+      switch (e.key) {
+        case "Enter":
+          reset();
+          return;
+      }
+    }
+
+    if (new_dir !== "none") {
+      if (new_dir !== oppositeDir.get(state.prev_dir) || snake.length <= 1) {
+        state.dir = new_dir;
+      }
+
+      if (state.status === "stopped") {
+        start();
+      }
+    }
+  }
+
+  $: lost = state.status === "lost";
 </script>
 
 <svelte:window on:keydown={handleInput} />
 
-<main>
-  {#if game.lost}
-    <h1 class="text-center">you lost</h1>
-    <h3 class="text-center">
-      Hit <code
-        class="border border-solid border-black bg-gray-200 rounded-sm p-1"
-        >ENTER</code
-      > to restart
-    </h3>
-  {/if}
-  <h3 class="text-center">score {game.grid.grid.length}</h3>
-  <div class="flex justify-center items-center">
-    <div>
-      {#each game.grid.grid as row, i}
-        <div class="flex">
-          {#each row as cell, j}
-            <!-- TODO: turn clicked cells into food? -->
-            <div class={`w-5 h-5 border border-solid border-white ${cell}`} />
-          {/each}
-        </div>
-      {/each}
-    </div>
+{#if lost}
+  <h1 class="text-center">you lost</h1>
+  <h3 class="text-center">
+    Hit <code
+      class="border border-solid border-black bg-gray-200 rounded-sm p-1"
+      >ENTER</code
+    > to restart
+  </h3>
+{/if}
+<h3 class="text-center">score {snake.length}</h3>
+<div class="flex justify-center items-center">
+  <div>
+    {#each grid as row, x}
+      <div class="flex">
+        {#each row as cell, y}
+          <!-- TODO: turn clicked cells into food? -->
+          <div
+            class={`w-5 h-5 border border-solid border-white ${cell.value}`}
+            on:click={() => (grid[x][y].food = "food")}
+          />
+        {/each}
+      </div>
+    {/each}
   </div>
+</div>
 
-  {#if game.lost}
-    <div class="center m-auto mt-3">
-      <button on:click={game.stop}> Start again </button>
-    </div>
-  {/if}
-</main>
+{#if lost}
+  <div class="center m-auto mt-3">
+    <button on:click={stop}> Start again </button>
+  </div>
+{/if}
 
 <style>
   .empty {
@@ -80,10 +232,10 @@
   .food {
     @apply bg-pink-500;
   }
-  .snake_body {
-    @apply bg-green-500;
-  }
-  .snake_head {
+  .body {
     @apply bg-green-800;
+  }
+  .head {
+    @apply bg-green-500;
   }
 </style>
